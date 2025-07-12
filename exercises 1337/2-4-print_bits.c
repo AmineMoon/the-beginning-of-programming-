48003f68059434693314857da3c52321664b97d1
AMINE EL ASSALY <amine.el.assaly.24@gmail.com>
2025-07-12 18:41:02 +0100

    Create  2-4-print_bits.c
    How It Works:
    print_bits function:
    
    Loops through each bit (from MSB to LSB)
    
    Uses bitwise operations to check each bit
    
    Prints "1" or "0" accordingly
    
    main function:
    
    Checks for correct number of arguments
    
    Takes first character of input string
    
    Prints the binary representation

---
+++ exercises 1337/2-4-print_bits.c
@@ -0,0 +1,27 @@
+#include <unistd.h>
+#include <stdlib.h>
+void print_bits(unsigned char octer ){
+ int i=8;
+  while(i--){
+ if (octer &  (1<<i))
+  write(1,"1",1);
+ else
+  write(1,"0",1);
+          }
+
+    }
+int main (int p , char**str){
+ if (p!=2){
+ write(1,"\n",1);
+return 0;
+}
+   // Convert first character of argument to byte
+    unsigned char input = (unsigned char)argv[1][0];
+  // convert string to number
+ // unsigned char input =(unsigned char) atoi (str[1]);
+ write(1,"binany :",8);
+
+print_bits(input);
+ write(1,"\n",1);
+ return 0;
+}
